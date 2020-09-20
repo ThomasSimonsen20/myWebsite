7d5a56e1c967e19d666187502a6f9ae28391e3db
Kameloozo <64187942+Kameloozo@users.noreply.github.com>
2020-09-20 14:01:59 +0200

    Tilføj projekt

---
+++ FfaOpgave/src/main/java/com/example/demo/Controller/MyController.java
@@ -0,0 +1,30 @@
+package com.example.demo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyController {
+
+    @GetMapping("/")
+    public String welcome() {
+        return "WelcomeScreen";
+    }
+
+    @GetMapping("/movies")
+    public String movies() {
+        return "Movies";
+    }
+
+    @GetMapping("/series")
+    public String series() {
+        return "Series";
+    }
+
+    @GetMapping("/babyDriver")
+    public String babyDriver() {
+        return "BabyDriver";
+    }
+
+
+}
